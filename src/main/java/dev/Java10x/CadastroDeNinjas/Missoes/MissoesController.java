42092082ac26174a327b49a0460279fe922f2e8d
Marcelo Barros <marcelobarrossilvafilho@gmail.com>
2025-05-09 16:17:38 -0300

    cria as rotas do controller com o mock

--- src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,5 +1,37 @@
 package dev.Java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    @PostMapping("/create")
+    public String criarMissao() {
+        return "Criar";
+    }
+    @GetMapping("/read")
+    public String mostrarTodasMissoes(){
+        return "Mostrar todos";
+    }
+    @GetMapping("/readID")
+    public String mostrarMissaoPorID(){
+        return "Mostrar por ID";
+    }
+    @PutMapping("/update")
+    public String alterarMissao() {
+        return "Alterar";
+    }
+    @DeleteMapping("/delete")
+    public String deltarMissao(){
+        return "Deletar";
+    }
+    
     
 }
--- src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -5,14 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 
 @RestController
-@RequestMapping
+@RequestMapping("ninjas")
 public class NinjaController {
 
     @GetMapping("/boasvindas")
@@ -25,11 +24,11 @@ public class NinjaController {
         return "Criar";
     }
     @GetMapping("/read")
-    public String mostrarTodos(){
+    public String mostrarTodosNinjas(){
         return "Mostrar todos";
     }
     @GetMapping("/readID")
-    public String mostrarPorID(){
+    public String mostrarNinjaPorID(){
         return "Mostrar por ID";
     }
     @PutMapping("/update")
